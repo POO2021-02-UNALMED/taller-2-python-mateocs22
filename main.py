3fa5acc361927661c4799eea3237be19399e6c5c
mateocs22 <mcarvajalsa@unal.edu.co>
2021-10-25 23:40:47 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,51 @@
+class Asiento:
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+
+    def cambiarColorL(self, color):
+        if(color in ("rojo", "verde", "amarillo", "negro", "blanco")):
+            self.color= color
+
+
+class Auto:
+    cantidadCreados= 0
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+
+    def cantidadAsientos(self):
+            c= 0
+            for i in self.asientos:
+                if type(i) == type(Asiento):
+                    c += 1 
+            return c
+
+    def verificarIntegridad(self):
+        rc= "Auto original"
+        re= "Las piezas no son originales"
+        for i in self.asientos:
+            if(i !=  None and i.registro != self.registro):
+                return re
+        if (self.registro == self.motor.registro):
+            return rc
+        else:
+            return re
+
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros= numeroCilindros
+        self.tipo= tipo
+        self.registro= registro
+
+    def CambiarRegistro(self, registro):
+        self.registro= registro
+
+    def asignarTipo(self, tipo):
+        if tipo in ("electrico", "normal"):
+            self.tipo= tipo
